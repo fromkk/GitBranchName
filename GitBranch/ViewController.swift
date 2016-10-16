87b64cbfca10de219e656fdd823552df342e7731
fromkk <info@fromkk.info>
2016-10-16 13:52:08 +0900

    Initial Commit

---
+++ GitBranch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitBranch
+//
+//  Created by Ueoka Kazuya on 2016/10/16.
+//  Copyright © 2016年 Fromkk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
